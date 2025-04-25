d4d5b06ebf21f46e9e8669735be3d400eb5dedbe
Sandeep Buddabathina <108016799+SandeepBuddabathina@users.noreply.github.com>
2025-04-25 12:13:17 +0530

    face added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,18 @@ import { FaceAuthComponent } from './face-auth/face-auth.component';
 import { FaceAuthGuard } from './face-auth.guard';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
- // { path: '', redirectTo: '/face-auth', pathMatch: 'full' },
-  {path:'',component:LoginComponent},
-  { path: 'login',component:LoginComponent},
-  { path: 'face-auth', component: FaceAuthComponent },
-  { path: 'video-call/:roomId', component: VideoCallComponent },
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-
+  { path: 'face-auth', component: FaceAuthComponent },
+  {
+    path: 'video-call/:roomId',
+    component: VideoCallComponent,
+    canActivate: [AuthGuard, FaceAuthGuard]     // ← add FaceAuthGuard here
+  },
 ];
 
 @NgModule({
